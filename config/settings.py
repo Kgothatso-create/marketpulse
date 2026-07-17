0932a0aebea23a2e88667deb7c26e4b304d14d1c
Kgothatso-create <kgothatsophooko@gmail.com>
2026-07-17 22:02:15 +0200

    Updated allowed list in settings

--- config/settings.py
+++ config/settings.py
@@ -15,12 +15,9 @@ import environ
 
 env = environ.Env()
 
-environ.Env.read_env()
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -31,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+ALLOWED_HOSTS = []
 
 
 # Application definition
